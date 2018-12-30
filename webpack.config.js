af7adeb79669d2a483ef0396a3bbd84d37bd6060
gabrielsinca <gabrielsinca14@gmail.com>
2018-12-30 21:36:44 +0200

    finished the front-end of the website

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        App: "./app/assets/scripts/App.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: "[name].js"
+    },
+    module: {
+        loaders:[
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
